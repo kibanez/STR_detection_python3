463502dc3f846b095b2f552dcb45da56625e81f7
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2019-11-22 13:34:55 +0000

    GRCh38 genomes that have been run with EHv3

--- visualisation/generating_pileup_plot_EHv3_b38.sh
+++ visualisation/generating_pileup_plot_EHv3_b38.sh
@@ -10,21 +10,29 @@
 # PyYAML (if visualizing output from versions older than v3) -- pip3.7 install pyyaml
 
 # Path to the binary python file
-GRAPH_SCRIPT=/Users/kibanez/git/GraphAlignmentViewer/GraphAlignmentViewer.py
-# Fasta file with .fai index for reference sequence. If not provided, flanks are set to 'N'. Default: None--reference_fasta REFERENCE_FASTA
-REFERENCE_FASTA=/Users/kibanez/Documents/human_reference/b38/genome.fa
-# Path to variant catalog used to run EH v3
-VARIANT_CATALOG_V3=/Users/kibanez/Documents/STRs/specs/EHv3.1.2/GRCh38/variant_catalog_GRCh38_12oct2019.json
-# Input folder where the json/vcf/log or bam files are
-INPUT_FOLDER=/Users/kibanez/Documents/STRs/ANALYSIS/random_pileups
-# Output folder where we want the plots
-OUTPUT_FOLDER=/Users/kibanez/Documents/STRs/ANALYSIS/random_pileups/plots
-# List of the platekeys to generate pileups
-list_ids=/Users/kibanez/Downloads/borratzeko/list_platekeys_poster.txt
+GRAPH_SCRIPT=/genomes/scratch/kgarikano/GEL_STR/STR_Visualization/GraphAlignmentViewer/GraphAlignmentViewer.py
 
+# Fasta file with .fai index for reference sequence. If not provided, flanks are set to 'N'. Default: None--reference_fasta REFERENCE_FASTA
+REFERENCE_FASTA=/genomes/resources/genomeref/Illumina/Homo_sapiens/NCBI/GRCh38Decoy/Sequence/WholeGenomeFasta/genome.fa
+
+# Path to variant catalog used to run EH v3
+VARIANT_CATALOG_V3=/genomes/scratch/kgarikano/GEL_STR/specs/EHv3.1.2/GRCh38/variant_catalog_GRCh38_12oct2019.json
+
+# Input folder where the json/vcf/log or bam files are
+INPUT_FOLDER=/home/dpasko/STRs/research_113K/EH_output_v3.1.2_October2019/
+
+# Output folder where we want the plots
+OUTPUT_FOLDER=/genomes/scratch/kgarikano/GEL_STR/STR_Visualization/random_plots/
+
+# List of the platekeys to generate pileups
+list_ids=/genomes/scratch/kgarikano/GEL_STR/STR_Visualization/list_platekey_EHv3_GRCh38.txt
+
+# Locus id
+LOCUS_ID=ATXN1
 
 # Load the virtual environment for dependencies
-source /Users/kibanez/git/GraphAlignmentViewer/venv/bin/activate
+source /genomes/scratch/kgarikano/GEL_STR/STR_Visualization/GraphAlignmentViewer/venv/bin/activate
+
 
 mkdir -p ${OUTPUT_FOLDER}
 cd ${OUTPUT_FOLDER}
@@ -32,18 +40,22 @@ cd ${OUTPUT_FOLDER}
 
 cat ${list_ids} | while read line; do
 
-    IFS=?~@~Y,?~@~Y read -ra NAMES <<< "$line" 
-    ID_NAME=${NAMES[0]}'.str_eh_v3'
+   IFS=?~@~Y,?~@~Y read -ra NAMES <<< "$line"
+    ID_NAME='EH_'${NAMES[0]}
 
     INPUT_BAM=${INPUT_FOLDER}${ID_NAME}'_realigned.bam'
-	  INPUT_VCF=${INPUT_FOLDER}${ID_NAME}'.vcf'
+    INPUT_VCF=${INPUT_FOLDER}${ID_NAME}'.vcf'
 
 
     python3 ${GRAPH_SCRIPT} \
     --variant_catalog ${VARIANT_CATALOG_V3} \
     --read_align ${INPUT_BAM} \
-    --vcf ${INPUT_VCF} \
+    --output_prefix ${ID_NAME} \
+    --output_dir ${OUTPUT_FOLDER} \
+    --dpi 600 \
+    --title_prefix ${ID_NAME}'_pileup' \
     --reference_fasta ${REFERENCE_FASTA} \
+    --locus_id ${LOCUS_ID} \
     --file_format v3
 
 done
