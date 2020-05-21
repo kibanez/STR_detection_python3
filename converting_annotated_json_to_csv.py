8ba7e884426697dbca8146020d31515d27feb34c
kristina ibanez garikano <kibanez.ingemm@gmail.com>
2020-05-21 16:13:00 +0100

    adapting code to GRCh37 annotation fields

--- converting_annotated_json_to_csv.py
+++ converting_annotated_json_to_csv.py
@@ -15,7 +15,12 @@ def print_tables(hash_table, f_output):
     :return:
     """
 
-    l_fields = ['chr', 'start', 'ref', 'alt', '1kG_phase3_ALL']
+    l_fields = ['chr', 'start', 'ref', 'alt', 'type', 'rsid', 'consequenceType',
+                'type', 'rsid', 'consequenceType', 'GEL.GL.5277', 'GEL.Platypus.RD.1777',
+                'GNOMAD_GENOMES_ALL', 'GNOMAD_GENOMES_AFR', 'GNOMAD_GENOMES_AMR', 'GNOMAD_GENOMES_EAS',
+                'GNOMAD_GENOMES_NFE', 'GNOMAD_GENOMES_FIN', 'GNOMAD_GENOMES_FEMALE', 'GNOMAD_GENOMES_MALE',
+                '1kG_phase3_ALL', '1kG_phase3_SAS', '1kG_phase3_AFR', '1kG_phase3_EUR', '1kG_phase3_AMR',
+                '1kG_phase3_EAS', 'UK10K_ALL', 'UK10K_TWINSUK_NODUP', 'UK10K_TWINSUK', 'ALSPAC']
 
     l_chr = set([item[0] for item in hash_table.keys()])
 
@@ -34,9 +39,9 @@ def print_tables(hash_table, f_output):
         l_chr.append(i)
 
     fo = open(f_output, 'w')
-    fo.write('\t'.join(l_fields) + '\n')
+    fo.write(','.join(l_fields) + '\n')
     for key in sorted(sorted(hash_table.keys(), key=itemgetter(1)), key=lambda x: l_chr.index(x[0])):
-        fo.write('\t'.join(map(lambda field: hash_table[key].get(field, '.'), l_fields)) + '\n')
+        fo.write(','.join(map(lambda field: hash_table[key].get(field, '.'), l_fields)) + '\n')
     fo.close()
 
 
@@ -57,23 +62,74 @@ with open(annotFile) as f:
                   annot["annotation"]["reference"] + "\t" + annot["annotation"]["alternate"]
 
         # Principal keys
-        #rsid = str(annot['id'])
         chr = str(annot['chromosome'])
         start = str(annot['start'])
         ref = str(annot['reference'])
         alt = str(annot['alternate'])
 
+        # Include single information
+        hash_variant['chr'] = chr
+        hash_variant['start'] = start
+        hash_variant['ref'] = ref
+        hash_variant['alt'] = alt
+        hash_variant['type'] = annot["type"]
+        hash_variant['rsid'] = annot.get('id', ".")
+
+        if "additionalAttributes" in annot["annotation"]:
+            if "GEL.GL.5277" in annot["annotation"]["additionalAttributes"]:
+                hash_variant['GEL.GL.5277'] = \
+                    annot["annotation"]["additionalAttributes"]["GEL.GL.5277"]["attribute"]["AF"]
+            if "GEL.Platypus.RD.1777" in annot["annotation"]["additionalAttributes"]:
+                hash_variant['GEL.Platypus.RD.1777'] = \
+                    annot["annotation"]["additionalAttributes"]["GEL.Platypus.RD.1777"]['attribute']['AF']
+        else:
+            hash_variant['GEL.GL.5277'] = '.'
+            hash_variant['GEL.Platypus.RD.1777'] = '.'
+
         # Extract CellBase annotation for GNOMAD_EXOMES-ALL
         if "populationFrequencies" in annot["annotation"] and len(annot["annotation"]["populationFrequencies"]) > 0:
             for pf in annot["annotation"]["populationFrequencies"]:
-                if pf["study"] == "1kG_phase3" and pf["population"] == "ALL":
-                    hash_variant['1kG_phase3_ALL'] = str(pf["altAlleleFreq"])
-                else:
-                    noStudy = "NO_STUDY"
-            if noStudy != "":
-                hash_variant['1kG_phase3_ALL'] = noStudy
-#        else:
-#            outLine += "\t" + "NOT_ANNOTATED"
+                if pf["study"] == "GNOMAD_GENOMES":
+                    if pf["population"] == "ALL":
+                        hash_variant['GNOMAD_GENOMES_ALL'] = str(pf["altAlleleFreq"])
+                    elif pf["population"] == "AFR":
+                        hash_variant['GNOMAD_GENOMES_AFR'] = str(pf["altAlleleFreq"])
+                    elif pf["population"] == "AMR":
+                        hash_variant['GNOMAD_GENOMES_AMR'] = str(pf["altAlleleFreq"])
+                    elif pf["population"] == "EAS":
+                        hash_variant['GNOMAD_GENOMES_EAS'] = str(pf["altAlleleFreq"])
+                    elif pf["population"] == "NFE":
+                        hash_variant['GNOMAD_GENOMES_NFE'] = str(pf["altAlleleFreq"])
+                    elif pf["population"] == "FIN":
+                        hash_variant['GNOMAD_GENOMES_FIN'] = str(pf["altAlleleFreq"])
+                    elif pf["population"] == "FEMALE":
+                        hash_variant['GNOMAD_GENOMES_FEMALE'] = str(pf["altAlleleFreq"])
+                    elif pf["population"] == "MALE":
+                        hash_variant['GNOMAD_GENOMES_MALE'] = str(pf["altAlleleFreq"])
+                elif pf["study"] == "1kG_phase3":
+                    if pf["population"] == "ALL":
+                        hash_variant['1kG_phase3_ALL'] = str(pf["altAlleleFreq"])
+                    elif pf["population"] == "SAS":
+                        hash_variant['1kG_phase3_SAS'] = str(pf["altAlleleFreq"])
+                    elif pf["population"] == "AFR":
+                        hash_variant['1kG_phase3_AFR'] = str(pf["altAlleleFreq"])
+                    elif pf["population"] == "EUR":
+                        hash_variant['1kG_phase3_EUR'] = str(pf["altAlleleFreq"])
+                    elif pf["population"] == "AMR":
+                        hash_variant['1kG_phase3_AMR'] = str(pf["altAlleleFreq"])
+                    elif pf["population"] == "EAS":
+                        hash_variant['1kG_phase3_EAS'] = str(pf["altAlleleFreq"])
+                elif pf["study"] == "UK10K":
+                    if pf["population"] == "ALL":
+                        hash_variant['UK10K_ALL'] = str(pf["altAlleleFreq"])
+                    elif pf["population"] == "TWINSUK_NODUP":
+                        hash_variant['UK10K_TWINSUK_NODUP'] = str(pf["altAlleleFreq"])
+                    elif pf["population"] == "TWINSUK":
+                        hash_variant['UK10K_TWINSUK'] = str(pf["altAlleleFreq"])
+                    elif pf["population"] == "ALSPAC":
+                        hash_variant['ALSPAC'] = str(pf["altAlleleFreq"])
+
+
         hash_table[(chr, start, ref, alt)] = hash_variant
 
 # Writing down hash_table into a file
